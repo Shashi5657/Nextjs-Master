b46593be4b6fa519c730a325e80dbdf9eb172f19
Shashi5657 <sangepushashidhar@gmail.com>
2024-07-02 13:27:15 +0530

    caching configuration

--- 00-news/app/(content)/news/[slug]/not-found.js
+++
@@ -1,8 +0,0 @@
-export default function NewsNotFound() {
-  return (
-    <div id="error">
-      <h1>Not Found</h1>
-      <p>We cannot find the requested News Article.</p>
-    </div>
-  );
-}
---
+++ 03-configure-caching/app/messages/page.js
@@ -0,0 +1,24 @@
+import Messages from '@/components/messages';
+// import {unstable_noStore} from 'next/cache'
+
+// export const revalidate = 5
+export const dynamic = 'force-dynamic'
+
+export default async function MessagesPage() {
+  // unstable_noStore()
+  const response = await fetch('http://localhost:8080/messages', {
+    // next : {
+    //   revalidate : 5
+    // }
+    // headers: {
+    //   'X-ID': 'page',
+    // },
+  });
+  const messages = await response.json();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
